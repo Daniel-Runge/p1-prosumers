4897ad25868d605378ffc56cc1f41cdcb79020ac
Raymond <rkacso20student.aau.dk>
2020-11-25 10:41:48 +0100

    Ray-test

--- test.c
+++ test.c
@@ -3,21 +3,20 @@
 #include <string.h>
 #include <curl/curl.h>
 
-int main (void)
+int main(void)
 {
-    
+
     printf("hello");
+    printf("Yes");
     CURL *curl;
     FILE *fp;
     int result;
-    
 
-    fp=fopen("testing.json", "wb");
+    fp = fopen("testing.json", "wb");
 
     curl = curl_easy_init();
     curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
     curl_easy_setopt(curl, CURLOPT_URL, "https://api.electricitymap.org/v3/power-breakdown/latest?zone=DK-DK1");
-
 
     struct curl_slist *headers = NULL;
     headers = curl_slist_append(headers, "auth-token: aRcMAViDADF2TuzMvUp3xFg6");
@@ -26,8 +25,6 @@ int main (void)
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
     result = curl_easy_perform(curl);
 
-
     fclose(fp);
     curl_easy_cleanup(curl);
-
 }
