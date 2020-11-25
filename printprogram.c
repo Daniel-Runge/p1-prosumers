f65eba18a6581084415d412e192e3ff8d5c2b3c3
sebastianbot6969 <saahol20@student.aau.dk>
2020-11-25 12:09:57 +0100

    I have written an placeholder to the start of the program

--- printprogram.c
+++ printprogram.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
-int main()
+#include <stdlib.h>
+int main(void)
 {
-    printf("dette er et print program din skøge");
-    return 0;
+    /*a welcome message in the making*/
+    printf("Hello love, welcome to our energy data program. This program can show you how Denmark " 
+    "produce their electrical energy right now. In Denmark we mainly use windpower to produce " 
+    "electricity, as windpower is not 100 procent consistent, the electrical power grid in Denmark "
+    "sometimes has surplus of energy, this means that Denmark sells its electricity to other countries, "
+    "and when the energy production is low, Denmark buys energy. This is all being displaced in this "
+    "program.");
+    return (0);
 }
