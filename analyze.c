82f5f7aed250944c84f076de4be54738eb21a6fa
MadsOverggard <nissum_10@hotmail.com>
2020-11-26 14:13:16 +0100

    Updated green power function to work with print

--- analyze.c
+++ analyze.c
@@ -4,39 +4,35 @@
 
 typedef struct /*Data we get from the API in MW |POWER CONSUMTION STRUCT|*/
 {
-    int battery_discharge;
-    int biomass;
-    int coal;
-    int gas;
-    int hydro;
-    int hydro_discharge;
-    int nuclear;
-    int oil;
-    int solar;
-    int wind;
-    int geothermal;
-    int unknown;
+    double battery_discharge;
+    double biomass;
+    double coal;
+    double gas;
+    double hydro;
+    double hydro_discharge;
+    double nuclear;
+    double oil;
+    double solar;
+    double wind;
+    double geothermal;
+    double unknown;
 } data_consumtion;
 
 typedef struct /*Data we get from the API in MW |TOTAL SECTION|*/
 {
-    int fossile;
-    int renewable;
-    int consumtionTotal;
-    int productionTotal;
-    int ImportTotal;
-    int ExportTotal;
+    double fossile;
+    double renewable;
+    double consumtionTotal;
+    double productionTotal;
+    double ImportTotal;
+    double ExportTotal;
+    double carbon_intensity;
+    double green;
 } data_total;
-
-typedef struct
-{
-    int carbon;
-    int green;
-}intensity;
 
 
 int analyze_data(data_total total, data_consumtion consumtion);
-void green_power(intensity *value);
+void green_power(data_total *value);
 
 /**
  * @brief Function to analyze data that comes from the API.
@@ -50,11 +46,11 @@ int main()
 {
     data_consumtion consumtion;
     data_total total;
-    intensity value;
+    
 
     /*Dummy data*/
 
-    value.carbon = 200;
+    total.carbon_intensity = 200;
 
     /*Data that was taken from an API Request |NOT UP TO DATE|*/
     total.fossile = 73;
@@ -72,9 +68,8 @@ int main()
     consumtion.wind = 763;
     consumtion.geothermal = 0;
     consumtion.unknown = 20;
-    
 
-    green_power(&value);
+    green_power(&total);
 
     return 0;
 }
@@ -97,14 +92,11 @@ int analyze_data(data_total total, data_consumtion consumtion)
     hydro_procentage = (consumtion.hydro * 100.) / total.consumtionTotal;     /*Precentage of total prower from Hydro*/
     biomass_procentage = (consumtion.biomass * 100.) / total.consumtionTotal; /*Precentage of total prower from Biomass*/
 
-
     printf("Solar:            %6.2lf %%\n", solar_procentage);
     printf("Wind:             %6.2lf %%\n", wind_procentage);
     printf("Hydro:            %6.2lf %%\n", hydro_procentage);
     printf("Biomass:          %6.2lf %%\n", biomass_procentage);
     printf("Sum af renewable: %6.2f %%\n", renewable_sum = wind_procentage + hydro_procentage + biomass_procentage + solar_procentage);
-
-
 
     /*How much CO2 in grams the different types of energy make in kWh (Kilo Watt Hour)*/
     /*1 MW = 1000 kWh*/
@@ -113,8 +105,6 @@ int analyze_data(data_total total, data_consumtion consumtion)
     /*Solar 45 gCO2/kWh*/
     /*Biomass 230 gCO2/kWh*/
     /*Coal 820 gCO2/kWh*/
-
-
 
     return 1;
 }
@@ -125,19 +115,18 @@ int analyze_data(data_total total, data_consumtion consumtion)
  * @param value The intensity struct as a pointer with the green value returen in the struct.
  */
 
-void green_power(intensity *value)
+void green_power(data_total *value)
 {
-    if (value->carbon <= 100)
+    if (value->carbon_intensity <= 100)
     {
-        value->green = 0;
+        return 0;
     }
-    else if (value->carbon <= 200)
+    else if (value->carbon_intensity <= 200)
     {
-        value->green = 1;
+        return 1;
     }
-    else if (value->carbon <= 300)
+    else if (value->carbon_intensity <= 300)
     {
-        value->green = 2;
+        return 2;
     }
 }
-
