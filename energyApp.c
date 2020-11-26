8b7b2e73e0515b318df5f158ac0282c3dd910eb6
Timumbaa <54961055+Timumbaa@users.noreply.github.com>
2020-11-26 12:23:13 +0100

    Update energyApp.c to have no functionality.

--- energyApp.c
+++ energyApp.c
@@ -8,13 +8,8 @@
 
 int main(void)
 {
-    CURL *curl;
-    CURLcode res;
-    FILE *fp;
-
     /* curl_global_init(CURL_GLOBAL_ALL); */
-    get_co2();
-    get_renevable();
 
     /*curl_global_cleanup();*/
+    return 0;
 }
