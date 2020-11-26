f44bf241f42a6a36739bd42991b4fafc145de6b0
Timumbaa <rungedaniel@hotmail.com>
2020-11-26 11:13:14 +0100

    Updated the Makefile to include header file. Reset the main c file energyApp.c.

--- Makefile
+++ Makefile
@@ -2,12 +2,13 @@ CC = gcc
 CFLAGS1 = -g
 CFLAGS2 = -g -c -Wall
 LIBS = `curl-config --libs`
+DEPS = energyAppFunctions.h
+
+%.o: %.c $(DEPS)
+	$(CC) -c -o $@ $< $(CFLAGS2)
 
 energyApp: energyApp.o
 	$(CC) $(CFLAGS1) -o $@ $< $(LIBS)
 
-energyApp.o: energyApp.c
-	$(CC) $(CFLAGS2) $<
-
 clean:
-	rm -f energyApp.o energyApp
+	rm -f *.o energyApp
--- energyApp.c
+++ energyApp.c
@@ -2,86 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
+#include "energyAppFunctions.h"
 
-struct string
-{
-    char *ptr;
-    size_t len;
-};
 
-void init_string(struct string *s)
-{
-    s->len = 0;
-    s->ptr = malloc(s->len + 1);
-    if (s->ptr == NULL)
-    {
-        fprintf(stderr, "malloc() failed\n");
-        exit(EXIT_FAILURE);
-    }
-    s->ptr[0] = '\0';
-}
-
-size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s)
-{
-    size_t new_len = s->len + size * nmemb;
-    s->ptr = realloc(s->ptr, new_len + 1);
-    if (s->ptr == NULL)
-    {
-        fprintf(stderr, "realloc() failed\n");
-        exit(EXIT_FAILURE);
-    }
-    memcpy(s->ptr + s->len, ptr, size * nmemb);
-    s->ptr[new_len] = '\0';
-    s->len = new_len;
-
-    return size * nmemb;
-}
 
 int main(void)
 {
     CURL *curl;
     CURLcode res;
     FILE *fp;
-    char filename[] = {"cattest.json"};
 
     curl_global_init(CURL_GLOBAL_ALL);
-
-    curl = curl_easy_init();
-    if (curl)
-    {
-        struct string s;
-        init_string(&s);
-
-        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_easy_setopt(curl, CURLOPT_URL, "https://api.electricitymap.org/v3/carbon-intensity/latest?zone=DK-DK1");
-
-        struct curl_slist *headers = NULL;
-        headers = curl_slist_append(headers, "auth-token: aRcMAViDADF2TuzMvUp3xFg6");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
-        res = curl_easy_perform(curl);
-
-        printf("%s\n", s.ptr);
-
-        fp = fopen(filename, "w+");
-
-        if (fp)
-        {
-            fputs(s.ptr, fp);
-        }
-        else
-        {
-            printf("Failed to open the file\n");
-        }
-
-        free(s.ptr);
-
-        /* always cleanup */
-        fclose(fp);
-        curl_easy_cleanup(curl);
-    }
+    get_co2();
+    get_renevable();
 
     curl_global_cleanup();
     return 0;
