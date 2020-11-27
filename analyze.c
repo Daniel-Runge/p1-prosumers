b55405a2842e44ebe88f6a4ef30e8fa8f69f0696
MadsOverggard <nissum_10@hotmail.com>
2020-11-27 09:55:07 +0100

    Updated with new function Wind Power

--- analyze.c
+++ analyze.c
@@ -3,27 +3,9 @@
 #include <stdlib.h>
 #include "energyAppFunctions.h"
 
-typedef struct /*Data we get from the API in MW |POWER CONSUMTION STRUCT|*/
-{
-    double battery_discharge;
-    double biomass;
-    double coal;
-    double gas;
-    double hydro;
-    double hydro_discharge;
-    double nuclear;
-    double oil;
-    double solar;
-    double wind;
-    double geothermal;
-    double unknown;
-} data_consumtion;
-
-
-
-
 int analyze_data(data_total total, data_consumtion consumtion);
 int green_power(data_total *value);
+int wind_power(data_total total, data_consumtion consumtion);
 
 /**
  * @brief Function to analyze data that comes from the API.
@@ -33,14 +15,16 @@ int green_power(data_total *value);
  * @return int To to use for the final print statement.
  */
 
-/* int main()
+int main()
 {
     data_consumtion consumtion;
     data_total total;
 
+    /*Dummy data*/
 
     total.carbon_intensity = 200;
 
+    /*Data that was taken from an API Request |NOT UP TO DATE|*/
     total.fossile = 73;
     total.renewable = 71;
     total.consumtionTotal = 2807;
@@ -57,11 +41,16 @@ int green_power(data_total *value);
     consumtion.geothermal = 0;
     consumtion.unknown = 20;
 
-    green_power(&total);
+    /*green_power(&total);*/
+
+    analyze_data(total, consumtion);
+
+    wind_power(total, consumtion);
+
+    printf("%d\n", wind_power(total, consumtion));
 
     return 0;
-} 
-*/
+}
 
 /**
  * @brief A function for futher use of analyzing data in the future.
@@ -87,6 +76,8 @@ int analyze_data(data_total total, data_consumtion consumtion)
     printf("Biomass:          %6.2lf %%\n", biomass_procentage);
     printf("Sum af renewable: %6.2f %%\n", renewable_sum = wind_procentage + hydro_procentage + biomass_procentage + solar_procentage);
 
+
+
     /*How much CO2 in grams the different types of energy make in kWh (Kilo Watt Hour)*/
     /*1 MW = 1000 kWh*/
     /*Wind 11 gCO2/kWh*/
@@ -99,9 +90,11 @@ int analyze_data(data_total total, data_consumtion consumtion)
 }
 
 /**
+ * 
  * @brief Function to get the index for the print statement is the power green?
  * 
  * @param value The intensity struct as a pointer with the green value returen in the struct.
+ * @return int Returns the 
  */
 
 int green_power(data_total *value)
@@ -118,4 +111,63 @@ int green_power(data_total *value)
     {
         return 2;
     }
+    else
+    {
+        return 3;
+    }
 }
+
+/**
+ * @brief A function to show if the wind is strong or not.
+ * 
+ * @param total Data from the total section of the API.
+ * @param consumtion Data from the consumtion section of the API.
+ * @return int Return 1 if wind power is 10% or under 2 if its over 50% and 0 if its anything else.
+ */
+
+int wind_power(data_total total, data_consumtion consumtion)
+{
+    int wind_procentage;
+    wind_procentage = (consumtion.wind * 100.) / total.consumtionTotal;
+
+    if (wind_procentage <= 10)
+    {
+        return 1;
+    }
+    else if (wind_procentage >= 50)
+    {
+        return 2;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/**
+ * @brief |WORK IN PROGRESS|
+ * 
+ * @param total 
+ * @param consumtion 
+ * @return int 
+ */
+
+int coal_power(data_total total, data_consumtion consumtion) 
+{
+    int coal_procentage;
+    coal_procentage = (consumtion.coal * 100.) / total.consumtionTotal;
+
+    if (coal_procentage >= 20)
+    {
+        return 1;
+    }
+    else if (coal_procentage <= 10)
+    {
+        return 2;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
