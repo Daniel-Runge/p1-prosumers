e9c3dd5123b86d0feabe9d6c7849a70655d23c56
nsare20 <nsare20@student.aau.dk>
2020-12-07 13:32:34 +0100

    Removed main and other functions, now ready for merge

--- forecast.c
+++ forecast.c
@@ -11,50 +11,12 @@ struct WindData
     double WindSpeed;
 };
 /* We get an array of size 48 from the API, this is size 50 for safety's sake */
-struct WindData WindPower[50];
 typedef struct WindData WindData;
-/* Prototypes */
-void get_api(char *filemode, char *url, char *filename, char *auth);
-void WeatherParser(char *Filename);
+WindData WindPower[50];
 
-/* Delete when file is complete */
-int main(void)
-{
-    get_api("w", "https://api.openweathermap.org/data/2.5/onecall?lat=56&lon=9.3&exclude=current,minutely,daily,alerts&appid=91f093992825e6f84a7a6f7033480686", "OpenWeatherMap.json", "91f093992825e6f84a7a6f7033480686");
-    WeatherParser("OpenWeatherMap.json");
-    return (0);
-}
+    /*get_api("w", "https://api.openweathermap.org/data/2.5/onecall?lat=56&lon=9.3&exclude=current,minutely,daily,alerts&appid=91f093992825e6f84a7a6f7033480686", "OpenWeatherMap.json", "91f093992825e6f84a7a6f7033480686");
+    WeatherParser("OpenWeatherMap.json");*/
 
-/* Delete when file is complete */
-void get_api(char *filemode, char *url, char *filename, char *auth)
-{
-
-    CURL *curl;
-    FILE *fp;
-    struct curl_slist *headers = NULL;
-    CURLcode result;
-
-    /* creates a file and a pointer to that file  */
-    fp = fopen(filename, filemode);
-
-    /* this is setting up the request type "GET" and the adress curl should access  */
-    curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-
-    /* creates a header file and puts the authentification token into it for the curl request  */
-    headers = curl_slist_append(headers, auth);
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
-    /*  tells curl that you want to write the data you get into your file */
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-    /* curl_easy_perform runs the curl that you set using easy_setopt */
-    result = curl_easy_perform(curl);
-
-    /* closes everything and cleans up */
-    fclose(fp);
-    curl_easy_cleanup(curl);
-}
 
 /**
  * @brief Takes a json file and parses it for our wanted data(Time and windspeed). 
@@ -68,8 +30,8 @@ void WeatherParser(char *Filename)
     char FileBuffer[15000];
     int i, NumOfHours;
     struct json_object *Full_json;
-    struct json_object *Hourly;
-    struct json_object *EveryHour;
+    struct json_object *AllHours;
+    struct json_object *SingleHour;
     struct json_object *Time;
     struct json_object *Wind;
     /*Opens file, reads data and puts data in 'FileBuffer'*/
@@ -83,21 +45,20 @@ void WeatherParser(char *Filename)
 
     /*Takes FileBuffer and puts it in Full_json struct, so it now is a json_object*/
     Full_json = json_tokener_parse(FileBuffer);
-    /*Looks for 'hourly' from json file and puts 'hourly' array into the 'Hourly' json_object*/
-    json_object_object_get_ex(Full_json, "hourly", &Hourly);
+    /*Looks for 'hourly' from json file and puts 'hourly' array into the 'AllHours' json_object*/
+    json_object_object_get_ex(Full_json, "hourly", &AllHours);
     /*Gives us the amount of hours we have forecasts for*/
-    NumOfHours = json_object_array_length(Hourly);
+    NumOfHours = json_object_array_length(AllHours);
     printf("%d\n", NumOfHours);
     /*for-loop puts data from every hour in correct place in the WindPower struct
     unixTime = Time
     WindSpeed = Wind*/
     for (i = 0; i < NumOfHours; i++)
     {
-        EveryHour = json_object_array_get_idx(Hourly, i);
-        json_object_object_get_ex(EveryHour, "dt", &Time);
-        json_object_object_get_ex(EveryHour, "wind_speed", &Wind);
+        SingleHour = json_object_array_get_idx(AllHours, i);
+        json_object_object_get_ex(SingleHour, "dt", &Time);
+        json_object_object_get_ex(SingleHour, "wind_speed", &Wind);
         WindPower[i].UnixTime = json_object_get_int(Time);
         WindPower[i].WindSpeed = json_object_get_double(Wind);
-        printf("%d - %lf\n", WindPower[i].UnixTime, WindPower[i].WindSpeed);
     }
-}
+} 
